d55769265346e37bc6d6e53657b281dc1d77e816
lena <albytova.elena@mail.ru>
2019-07-06 18:59:52 +0300

    Updates

--- precache-manifest.c1bbb193ba9cb964fbe4583ea69e57d7.js
+++ precache-manifest.c1bbb193ba9cb964fbe4583ea69e57d7.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/vk-costbus/static/js/runtime~main.8e96b4a3.js"
   },
   {
-    "revision": "46f93409e8244daf44a4",
-    "url": "/vk-costbus/static/js/main.46f93409.chunk.js"
+    "revision": "539e7805c1e6218099df",
+    "url": "/vk-costbus/static/js/main.539e7805.chunk.js"
   },
   {
-    "revision": "2cefd87e94cdd227cfbe",
-    "url": "/vk-costbus/static/js/2.2cefd87e.chunk.js"
+    "revision": "075f8891903a6734083a",
+    "url": "/vk-costbus/static/js/2.075f8891.chunk.js"
   },
   {
-    "revision": "46f93409e8244daf44a4",
+    "revision": "539e7805c1e6218099df",
     "url": "/vk-costbus/static/css/main.229098c0.chunk.css"
   },
   {
-    "revision": "2cefd87e94cdd227cfbe",
+    "revision": "075f8891903a6734083a",
     "url": "/vk-costbus/static/css/2.012b87b6.chunk.css"
   },
   {
-    "revision": "e423bfc5fc345b251a4ff801d365444b",
+    "revision": "13f9a6c2964458f256bc9845e68e85e7",
     "url": "/vk-costbus/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-costbus/precache-manifest.34c4cbb9328312284ebbbfc0af8a2e9d.js"
+  "/vk-costbus/precache-manifest.c1bbb193ba9cb964fbe4583ea69e57d7.js"
 );
 
 workbox.clientsClaim();
