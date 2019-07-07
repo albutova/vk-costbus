ea3dea1506940921c21be791f580091726f6dce9
lena <albytova.elena@mail.ru>
2019-07-07 16:19:21 +0300

    Updates

--- precache-manifest.2d709f27bfdc9fb09348ea8d2b345652.js
+++ precache-manifest.2d709f27bfdc9fb09348ea8d2b345652.js
@@ -4,8 +4,8 @@ self.__precacheManifest = [
     "url": "/vk-costbus/static/js/runtime~main.8e96b4a3.js"
   },
   {
-    "revision": "eba6c8f430a1ebb66989",
-    "url": "/vk-costbus/static/js/main.eba6c8f4.chunk.js"
+    "revision": "63adf811e98187495a15",
+    "url": "/vk-costbus/static/js/main.63adf811.chunk.js"
   },
   {
     "revision": "6bfe913375043702853b",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/vk-costbus/static/css/2.012b87b6.chunk.css"
   },
   {
-    "revision": "e62324c9429d3b0e9c70045af5216613",
+    "revision": "ea34740372976a3ae4a05bcf59a9b21c",
     "url": "/vk-costbus/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-costbus/precache-manifest.4fd225a2e830c3a7fb80662428df0e9a.js"
+  "/vk-costbus/precache-manifest.2d709f27bfdc9fb09348ea8d2b345652.js"
 );
 
 workbox.clientsClaim();
