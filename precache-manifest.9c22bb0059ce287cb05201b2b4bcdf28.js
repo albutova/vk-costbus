0307836bf7d98a2648947e4297db8af7ee6bd343
lena <albytova.elena@mail.ru>
2019-08-06 22:45:45 +0300

    Updates

--- precache-manifest.9c22bb0059ce287cb05201b2b4bcdf28.js
+++ precache-manifest.9c22bb0059ce287cb05201b2b4bcdf28.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/vk-costbus/static/js/runtime~main.8e96b4a3.js"
   },
   {
-    "revision": "f7d8c9192597f91645a6",
-    "url": "/vk-costbus/static/js/main.f7d8c919.chunk.js"
+    "revision": "c6debdb0cd0d0125fb43",
+    "url": "/vk-costbus/static/js/main.c6debdb0.chunk.js"
   },
   {
     "revision": "59825de71846458edcb5",
     "url": "/vk-costbus/static/js/2.59825de7.chunk.js"
   },
   {
-    "revision": "f7d8c9192597f91645a6",
+    "revision": "c6debdb0cd0d0125fb43",
     "url": "/vk-costbus/static/css/main.44ed3889.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/vk-costbus/static/css/2.012b87b6.chunk.css"
   },
   {
-    "revision": "a511acf5a4a3739ea32481cbb2923ca6",
+    "revision": "be7bcf3389e18a47997b0716488900e4",
     "url": "/vk-costbus/index.html"
   }
 ];
